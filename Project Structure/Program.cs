ac3a6ab87195f88a3c9afe65d21db34335ac0d7b
Mena Erian <mena3koko2karas1@gmail.com>
2025-09-20 21:35:40 +0300

    Add project files.

---
+++ Project Structure/Program.cs
@@ -0,0 +1,171 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections;
+
+namespace Project_Structure
+{
+    /// class ProductsControllers : Controller
+    /// {
+    ///     private ProductService _productService;
+    /// 
+    ///     public ProductsControllers(ProductService productService)
+    ///     {
+    ///         _productService = productService;
+    ///     }
+    /// 
+    ///     // Action: baseUrl/Prodcuts/GetProduct?id = 10
+    ///     public IActionResult GetProduct(int id)
+    ///     {
+    ///         var product = _productService.GetProductById(id);
+    ///         return View(product);
+    ///     }
+    /// 
+    /// }
+    /// 
+    /// class ProductService
+    /// {
+    ///     private ProductRepository _productRepository;
+    ///     private CategoryRepository _categoryRepository;
+    /// 
+    ///     public ProductService(ProductRepository productRepository, CategoryRepository categoryRepository)
+    ///     {
+    ///         _productRepository = productRepository;
+    ///         _categoryRepository = categoryRepository;
+    ///     }
+    /// 
+    ///     public Product GetProductById(int id)
+    ///     {
+    /// 
+    ///         var categories = _categoryRepository.GetAll().FindAll(c => c.IsOutOfStock == false);
+    ///         var product = _productRepository.Get(id);
+    /// 
+    ///         if (!categories.Contains(product.Category)) return product;
+    /// 
+    ///         return null;
+    /// 
+    ///         // Business Logic
+    ///     }
+    /// }
+    /// class ProductRepository
+    /// {
+    ///     private ApplicationDbContext _dbContext;
+    /// 
+    ///     public ProductRepository(ApplicationDbContext dbContext) // ASk CLR for creating Object from dbCOntext
+    ///     {
+    ///         _dbContext = dbContext;
+    ///     }
+    /// 
+    ///     public Product Get(int id)
+    ///     {
+    ///         return _dbContext.Prodcuts.Find(id);
+    ///     }
+    /// }
+    /// class CategoryRepository
+    /// {
+    ///     private ApplicationDbContext _dbContext;
+    /// 
+    ///     public CategoryRepository(ApplicationDbContext dbContext)
+    ///     {
+    ///         _dbContext = dbContext;
+    ///     }
+    /// 
+    ///     public List<Category> GetAll()
+    ///     {
+    ///         return _dbContext.Categories.ToList();
+    ///     }
+    /// }
+    /// class Product
+    /// {
+    ///     public int Id { get; set; }
+    ///     public string? Name { get; set; }
+    ///     public string? Description { get; set; }
+    ///     public decimal Price { get; set; }
+    ///     public Category Category { get; set; }
+    /// }
+    /// class Category
+    /// {
+    ///     public int Id { get; set; }
+    ///     public string? Name { get; set; }
+    ///     public bool IsOutOfStock { get; set; }
+    /// 
+    /// }
+    /// class ApplicationDbContext : DbContext
+    /// {
+    ///     public ApplicationDbContext(DbContextOptions options) : base(options)
+    ///     {
+    /// 
+    ///     }
+    ///     public DbSet<Product> Prodcuts { get; set; }
+    ///     public DbSet<Category> Categories { get; set; }
+    /// 
+    /// }
+
+
+    public class Program
+    {
+        // Entry Point
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder();
+            builder.Services.AddControllersWithViews();
+
+
+
+            var app = builder.Build();
+
+
+
+
+            /// builder.Services.AddScoped<ApplicationDbContext>(option => option.UseSqlServer("ConnectionStr"));    // use one object as long as you in the same request
+            /// builder.Services.AddScoped<ProductRepository>();
+            /// builder.Services.AddScoped<CategoryRepository>();
+            /// builder.Services.AddScoped<ProductService>();
+
+
+            //app.UseRouting();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.MapPost("/Hamada", async (context) =>
+            {
+                await context.Response.WriteAsync("Hello Hamada");
+            });
+
+            app.MapGet("/test", async (context) =>
+            {
+                await context.Response.WriteAsync("{ a: 1 }");
+            });
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller}/{action}/{id?}"
+                );
+
+
+            if(app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseStatusCodePagesWithReExecute("/Home/Error");
+            }
+            app.UseRouting();
+
+         
+
+                app.Run();
+
+        }
+        /// public void Config(IServiceCollection services)
+        /// {
+        ///     //services.AddTransient<ApplicationDbContext>(); // add new object for each 
+        ///     //services.AddSingleton<ApplicationDbContext>(); // create object just one and it will live in heap as long as you open connection
+        ///     services.AddScoped<ApplicationDbContext>(option => option.UseSqlServer("ConnectionStr"));    // use one object as long as you in the same request
+        ///     services.AddScoped<ProductRepository>();
+        ///     services.AddScoped<CategoryRepository>();
+        ///     services.AddScoped<ProductService>();
+        ///
+        /// }
+    }
+}
---
+++ test/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main()
+{
+	int  test = 3;
+	printf("Welcome %d",test);
+
+
+	getch();
+}
